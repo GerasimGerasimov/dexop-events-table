6fa1e8fa4370d9359460d215d2fcb9b14d6a6a67
Gerasim Gerasimov <dialix@yandex.ru>
2020-09-28 22:13:15 +0700

    убрал лишнее

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import './App.css';
 import EventsTable, { IEventItem } from './components/table/EventsTable';
 import { dataset } from './datasets/events';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <EventsTable items={Object.entries(dataset).map((value, index, array)=>{
         const id = value[0] || ''
         const {tag, datetime, details} = {... value[1]};
