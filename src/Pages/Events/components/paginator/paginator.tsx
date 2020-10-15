9ab2b5348028e44ac3f1f5408242935089b03480
Gerasim Gerasimov <dialix@yandex.ru>
2020-10-15 15:11:52 +0700

    оформление кнопок в пагинаторе

--- src/Pages/Events/components/paginator/paginator.tsx
+++ src/Pages/Events/components/paginator/paginator.tsx
@@ -65,16 +65,20 @@ export default class Paginator extends Component <IPaginatorProps,IPaginatorStat
       <div className='flex alitcn jcsa'>
         <span>{this.props.ItemsBefore}</span>
         <button
+          className="btn btn-primary"
           disabled={this.isDisabled(this.props.ItemsBefore)}
           onClick={()=>this.props.nextItemsHandler(IEventQueryDirection.Prev)}>Pred</button>
         <button
+          className="btn btn-primary"
           disabled={this.isDisabled(this.props.ItemsAfter)}
           onClick={()=>this.props.nextItemsHandler(IEventQueryDirection.Next)}>Next</button>
         <span>{this.props.ItemsAfter}</span>
         <button
+          className="btn btn-secondary"
           disabled={this.state.subDisabled}
           onClick={()=>this.props.setNumberOfItemsOnPageHandler(this.subItemsOnPage(this.props.ItemsPortion))}>-10</button>       
         <button
+          className="btn btn-secondary"
           disabled={this.state.addDisabled}
           onClick={()=>this.props.setNumberOfItemsOnPageHandler(this.addItemsOnPage(this.props.ItemsPortion))}>+10</button>
      </div>
