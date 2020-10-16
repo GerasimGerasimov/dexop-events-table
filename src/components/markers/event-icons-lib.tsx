b7846a1ed58f7caac8a5b8ab84ff2c8ad8be5cec
Gerasim Gerasimov <dialix@yandex.ru>
2020-10-16 22:26:02 +0700

    немного увеличил размеры SVG картинок

--- src/components/markers/event-icons-lib.tsx
+++ src/components/markers/event-icons-lib.tsx
@@ -17,9 +17,9 @@ export function getEventIconsByType(type: string): JSX.Element {
 function getIconAlarm(): JSX.Element{
   return (
     <svg className='_svg' viewBox="0 0 1 1">
-      <g transform='translate(0.15, 0.15) scale(0.85)'>
+      <g transform='translate(0.05, 0.05) scale(0.95)'>
         <path d="M 0 0.5 L 0.5 0 L 1 0.5 L 0.5 1 Z"
-          fill="red"/>
+          fill="red" />
       </g>
     </svg>
   )
@@ -28,7 +28,7 @@ function getIconAlarm(): JSX.Element{
 function getIconWarning(): JSX.Element{
   return (
     <svg className='_svg' viewBox="0 0 1 1">
-      <g transform='translate(0.15, 0.15) scale(0.85)'>
+      <g transform='translate(0.05, 0.05) scale(0.95)'>
         <rect x='0' y='0' width='1' height='1'
           fill="#ffba00"/>
       </g>
@@ -39,7 +39,7 @@ function getIconWarning(): JSX.Element{
 function getIconInfo(): JSX.Element{
   return (
     <svg className='_svg' viewBox="0 0 1 1">
-      <g transform='translate(0.15, 0.15) scale(0.85)'>
+      <g transform='translate(0.05, 0.05) scale(0.95)'>
         <path d="M 0 1 L 0.5 0 L 1 1 Z"
           fill="#8000C0"/>
       </g>
